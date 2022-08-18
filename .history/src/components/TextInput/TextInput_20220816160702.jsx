4ee20b5e0e3faca204fd2d5b92742e1b494cb9e5
Mariana Luiza Vitor Alves Silva <mariana.luiza@spcbrasil.org.br>
2022-08-18 14:57:22 -0300

    adicionando projeto no repositorio github

---
+++ .history/src/components/TextInput/TextInput_20220816160702.jsx
@@ -0,0 +1,20 @@
+import P from 'prop-types';
+
+import './Text.css';
+
+export const TextInput = ({ searchValue, handleChange }) => {
+  return (
+    <input
+      className="text-input"
+      onChange={handleChange}
+      value={searchValue}
+      type="search"
+      placeholder="Type your search"
+    />
+  );
+};
+
+TextInput.propTypes = {
+  searchValue: P.string.isRequired,
+  handleChange: P.func.isRequired,
+};
